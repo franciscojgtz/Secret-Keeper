d3675f39443b287cd381ca0d8c6256d8fee71c2d
Francisco Gutierrez <francisco.j.gtz@gmail.com>
2019-09-18 14:27:19 -0700

    feat: Add Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 const App: React.FC = () => {
-  return <div className="App">Secret-Keeper</div>;
+  return (
+    <Container className="App">
+      <Row>
+        <Col xs={4}>Guesses Left</Col>
+        <Col xs={8}>Message Board</Col>
+      </Row>
+      <Row>
+        <Col xs={4}>Incorrect Guesses</Col>
+        <Col xs={8}>Guess Letter Input /></Col>
+      </Row>
+      <Row>
+        <Col>Secret Word</Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default App;
